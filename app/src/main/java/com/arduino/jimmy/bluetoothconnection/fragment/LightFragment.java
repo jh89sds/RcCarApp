5b20ad0a97d87ded865ec8d18559050d2eb00017
Jimmy <ljh89st@gmail.com>
2017-09-04 23:28:24 +0900

    light on off add

--- app/src/main/java/com/arduino/jimmy/bluetoothconnection/fragment/LightFragment.java
+++ app/src/main/java/com/arduino/jimmy/bluetoothconnection/fragment/LightFragment.java
@@ -1,8 +1,65 @@
 package com.arduino.jimmy.bluetoothconnection.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arduino.jimmy.bluetoothconnection.R;
+
 /**
  * Created by Jimmy on 2017-09-03.
  */
 
 public class LightFragment extends TabFragment {
+
+    private View inflatedView;
+
+    private View frontLightOn;
+    private View frontLightOff;
+    private View rearLightOn;
+    private View rearLightOff;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        inflatedView = inflater.inflate(R.layout.light_layout, container, false);
+
+        setButtons();
+
+        return inflatedView;
+    }
+
+    private void setButtons() {
+        frontLightOn = inflatedView.findViewById(R.id.front_light_on);
+        frontLightOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData("1");
+            }
+        });
+
+        frontLightOff = inflatedView.findViewById(R.id.front_light_off);
+        frontLightOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData("3");
+            }
+        });
+
+        rearLightOn = inflatedView.findViewById(R.id.rear_light_on);
+        rearLightOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData("2");
+            }
+        });
+
+        rearLightOff = inflatedView.findViewById(R.id.rear_light_off);
+        rearLightOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData("4");
+            }
+        });
+    }
 }
--- app/src/main/java/com/arduino/jimmy/bluetoothconnection/fragment/TabFragment.java
+++ app/src/main/java/com/arduino/jimmy/bluetoothconnection/fragment/TabFragment.java
@@ -1,7 +1,53 @@
 package com.arduino.jimmy.bluetoothconnection.fragment;
 
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import com.arduino.jimmy.bluetoothconnection.SocketSingleton;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class TabFragment extends Fragment {
 
+    protected
+    BluetoothSocket mSocket;
+
+    protected OutputStream mOutputStream;
+    protected InputStream mInputStream;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setBluetoothConnection();
+    }
+
+    protected void sendData(String msg) {
+        try{
+            // getBytes() : String을 byte로 변환
+            // OutputStream.write : 데이터를 쓸때는 write(byte[]) 메소드를 사용함. byte[] 안에 있는 데이터를 한번에 기록해 준다.
+            mOutputStream.write(msg.getBytes());  // 문자열 전송.
+        }catch(Exception e) {  // 문자열 전송 도중 오류가 발생한 경우
+            Toast.makeText(getActivity(), "데이터 전송중 오류가 발생", Toast.LENGTH_LONG).show();
+            getActivity().finish();  // App 종료
+        }
+    }
+
+    private void setBluetoothConnection() {
+        try {
+            // 데이터 송수신을 위한 스트림 얻기.
+            // BluetoothSocket 오브젝트는 두개의 Stream을 제공한다.
+            // 1. 데이터를 보내기 위한 OutputStrem
+            // 2. 데이터를 받기 위한 InputStream
+            mSocket = SocketSingleton.getSocket();
+            mOutputStream = mSocket.getOutputStream();
+            mInputStream = mSocket.getInputStream();
+        } catch (IOException e) {
+            Toast.makeText(getActivity(), "블루투스가 연결되지 않았습니다.", Toast.LENGTH_LONG).show();
+        }
+    }
 }
